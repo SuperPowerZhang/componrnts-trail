da8870fe54b9474b58df933a5c775b976dcf225d
张无忌 <2285546178@qq.com>
2022-05-02 17:40:39 +0800

    use parcel and extract button as a template

---
+++ src/button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="y-button">按钮</button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+.y-button {
+  height: var(--button-height);
+  font-size: var(--font-size);
+  padding: 0 1em;
+  border: 1px solid #3e504d;
+  border-radius: var(--border-radius);
+  color: var(--button-border-color);
+  background-color: var(--default--bg);
+}
+.y-button:hover {
+  border-color: var(--button-hover-border);
+}
+.y-button:active {
+  background-color: var(--button-active-bg);
+}
+</style>
