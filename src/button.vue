6637e2a22ea80b242de4d95ac0f54871d34c9028
张无忌 <2285546178@qq.com>
2022-05-02 18:13:13 +0800

    use icon

--- src/button.vue
+++ src/button.vue
@@ -1,13 +1,23 @@
 <template>
-  <button class="y-button">按钮</button>
+  <button class="y-button">
+    <svg class="icon" aria-hidden="true" v-if="icon">
+      <use :xlink:href="`#i-${icon}`"></use></svg
+    ><slot />
+  </button>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["icon"],
+};
 </script>
 
 <style scoped lang="scss">
 .y-button {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
   height: var(--button-height);
   font-size: var(--font-size);
   padding: 0 1em;
@@ -15,6 +25,9 @@ export default {};
   border-radius: var(--border-radius);
   color: var(--button-border-color);
   background-color: var(--default--bg);
+  .icon {
+    margin-right: 0.5em;
+  }
 }
 .y-button:hover {
   border-color: var(--button-hover-border);
